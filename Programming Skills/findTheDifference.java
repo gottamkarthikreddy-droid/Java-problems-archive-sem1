badd27597d030fe98b949d152c352103e0805e12
Karthik Reddy <gottamkarthikreddy@klh.edu.in>
2026-09-11 21:14:32 +0530

    Added programming skills

---
+++ Programming Skills/findTheDifference.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        
+        int sumone = 0;
+        int sumtwo = 0;
+
+        for(int i = 0; i<s.length();i++)
+        {
+            sumone = sumone + s.charAt(i);
+        }
+        for(int i = 0; i<t.length();i++)
+        {
+            sumtwo = sumtwo + t.charAt(i);
+        }
+        return (char)(sumtwo-sumone);
+        
+    }
+}
